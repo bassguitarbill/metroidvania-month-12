706a4bcf619a0a7f85050ec2132ecf17bf3a1433
bassguitarbill <bassguitarbill@gmail.com>
2021-05-21 19:09:11 -0400

    Clamp the camera to the middle of narrow rooms

--- src/Camera.ts
+++ src/Camera.ts
@@ -8,8 +8,24 @@ export default class Camera {
   constructor(readonly game: Game) {};
 
   translateCamera(ctx: CanvasRenderingContext2D) {
-    this.translationX = PLAYFIELD_WIDTH/2 - this.game.player.x;
-    this.translationY = PLAYFIELD_HEIGHT/2 - this.game.player.y;
+    const zone = this.game.gameMap!.getZone(this.game.player.position);
+    if (!zone) {
+      this.translationX = PLAYFIELD_WIDTH/2 - this.game.player.x;
+      this.translationY = PLAYFIELD_HEIGHT/2 - this.game.player.y;
+    } else {
+      const verticallyConstrained = zone.height <= PLAYFIELD_HEIGHT;
+      const horizontallyConstrained = zone.width <= PLAYFIELD_WIDTH;
+      if (verticallyConstrained) {
+        this.translationY = PLAYFIELD_HEIGHT/2 - zone.y - (zone.height / 2);
+      } else {
+        this.translationY = PLAYFIELD_HEIGHT/2 - this.game.player.y;
+      }
+      if (horizontallyConstrained) {
+        this.translationX = PLAYFIELD_WIDTH/2 - zone.x - (zone.width / 2);
+      } else {
+        this.translationX = PLAYFIELD_WIDTH/2 - this.game.player.x;
+      }
+    }
     ctx.translate(this.translationX, this.translationY);
   }
 
